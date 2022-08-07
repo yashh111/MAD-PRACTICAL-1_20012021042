20dfe2e85124035dcdd97ee9447676039c53ca0e
yash <lakhaniyash111@gmail.com>
2022-08-07 13:58:47 +0530

    pr-1

---
+++ src/main/kotlin/practical1.kt
@@ -0,0 +1,22 @@
+fun main()
+{
+    val x=22
+    println("x=$x")
+    val y=1.5f
+    println("y=$y")
+    var z:Char = 'T'
+    println("z=$z")
+    var name:String
+    name= "HMS"
+    println(name)
+    var a:Boolean = false
+    println("a=$a")
+    val b:Double = 22.22
+    println("b=$b")
+    val c:Long = 465132345
+    println("c=$c")
+    val d:Short = 1
+    println("d=$d")
+    val e:Byte = 125
+    println("e=$e")
+}
